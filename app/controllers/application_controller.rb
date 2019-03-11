c50318a1feee41838c1504d9bbbfcddcbe0f8b3a
Anushka Fernando <anushka.rf@gmail.com>
2019-03-11 23:13:15 +1100

    API call and caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  def get(url, params)
+    Rails.cache.fetch([url, params], :expires => 1.hour) do
+      HTTParty.get(url)
+    end
+  end
 end
